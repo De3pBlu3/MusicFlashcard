ac0b84f733d7390f243dfc3c83c6d63b5d5df78f
23357614 <jackcasey614@gmail.com>
2023-11-28 15:56:08 +0000

    removed comments

--- FXMLloader.java
+++ FXMLloader.java
@@ -6,9 +6,7 @@ import java.io.IOException;
 public class FXMLloader {
     public static Scene loadScene(String url) throws IOException {
         FXMLLoader loader = new FXMLLoader();
-//        loader.setLocation(new URL(url));
         loader.setLocation(FXMLloader.class.getResource(url));
-        // gridpane is the name of the fx:id of the root element in the fxml file
         VBox root = loader.load();
         return new Scene(root);
     }

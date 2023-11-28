1142cb5a278ca4e3373386fddd04246d0e75a45e
23357614 <jackcasey614@gmail.com>
2023-11-28 15:57:09 +0000

    clean up IO input, refactor methods to clean up logic

--- FXMLquestionio.java
+++ FXMLquestionio.java
@@ -32,42 +32,30 @@ public class FXMLquestionio {
     public void initialize(){
         // make sure only one difficulty is selected
         easyButton.setOnAction(e -> {
-            if (easyButton.isSelected()) {
-                mediumButton.setSelected(false);
-                hardButton.setSelected(false);
-            }
+            mediumButton.setSelected(false);
+            hardButton.setSelected(false);
         });
         mediumButton.setOnAction(e -> {
-            if (mediumButton.isSelected()) {
-                easyButton.setSelected(false);
-                hardButton.setSelected(false);
-            }
+            hardButton.setSelected(false);
+            easyButton.setSelected(false);
         });
         hardButton.setOnAction(e -> {
-            if (hardButton.isSelected()) {
-                mediumButton.setSelected(false);
-                easyButton.setSelected(false);
-            }
+            mediumButton.setSelected(false);
+            easyButton.setSelected(false);
         });
 
         // make sure only one topic is selected
         csButton.setOnAction(e -> {
-            if (csButton.isSelected()) {
-                dmButton.setSelected(false);
-                comporgButton.setSelected(false);
-            }
+            dmButton.setSelected(false);
+            comporgButton.setSelected(false);
         });
         dmButton.setOnAction(e -> {
-            if (dmButton.isSelected()) {
-                csButton.setSelected(false);
-                comporgButton.setSelected(false);
-            }
+            csButton.setSelected(false);
+            comporgButton.setSelected(false);
         });
         comporgButton.setOnAction(e -> {
-            if (comporgButton.isSelected()) {
-                dmButton.setSelected(false);
-                csButton.setSelected(false);
-            }
+            dmButton.setSelected(false);
+            csButton.setSelected(false);
         });
 
         returnButton.setOnAction(e -> {
@@ -82,42 +70,54 @@ public class FXMLquestionio {
         // add question to database
         addButton.setOnAction(e -> {    // TODO error checking
 
-            int rand = (int)(Math.random() * 500)+1; // TODO, this is stupid, fix it
-            // string
-            String card_id= "usr" + rand;
-            // get difficulty
-            int difficulty = 0;
-            if (easyButton.isSelected()) {
-                difficulty = 1;
-            } else if (mediumButton.isSelected()) {
-                difficulty = 2;
-            } else if (hardButton.isSelected()) {
-                difficulty = 3;
-            }
 
-            // get topic
-            int topic = 0;
-            if (csButton.isSelected()) {
-                topic = 1;
-            } else if (dmButton.isSelected()) {
-                topic = 2;
-            } else if (comporgButton.isSelected()) {
-                topic = 3;
-            }
-            card user_manual = new card(card_id, questionField.getText(), correctField.getText() + ";" + inc1Field.getText() + ";" + inc2Field.getText() + ";" + inc3Field.getText(), 0, topic, difficulty);
+            // user ID
+            card user_manual = getCardfromUser();
             DB_CardInteract.addCardToDB(user_manual);
+
+            // create popup to confirm question added
             Popup successfulQAddp = new Popup();
             Label successfulQAddpLabel = new Label("Question Successfully added");
             successfulQAddp.getContent().add(successfulQAddpLabel);
-            // add padding to bottom of popup
             successfulQAddpLabel.setPadding(new javafx.geometry.Insets(0, 0, 150, 0));
-
             SuccessPopUp(successfulQAddp, (Stage) returnButton.getScene().getWindow());
 
 
         });
 
     }
+
+    private card getCardfromUser() {
+        int rand = (int)(Math.random() * 500)+1; // TODO, this is stupid, fix it
+
+        String card_id= "usr" + rand;
+
+
+        // get difficulty
+        int difficulty = 0;
+        if (easyButton.isSelected()) {
+            difficulty = 1;
+        } else if (mediumButton.isSelected()) {
+            difficulty = 2;
+        } else if (hardButton.isSelected()) {
+            difficulty = 3;
+        }
+
+        // get topic
+        int topic = 0;
+        if (csButton.isSelected()) {
+            topic = 1;
+        } else if (dmButton.isSelected()) {
+            topic = 2;
+        } else if (comporgButton.isSelected()) {
+            topic = 3;
+        }
+
+        // create card and add to DB
+        card user_manual = new card(card_id, questionField.getText(), correctField.getText() + ";" + inc1Field.getText() + ";" + inc2Field.getText() + ";" + inc3Field.getText(), 0, topic, difficulty);
+        return user_manual;
+    }
+
     public static void SuccessPopUp(Popup successfulQAddp, Stage stage) {
         Timeline popupTimeline = new Timeline( //To store keyframes
                 new KeyFrame(Duration.seconds(0), e -> {
